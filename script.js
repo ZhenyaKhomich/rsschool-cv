fb23c07ca284e8ab3dc343d4f49bb715a4f6b0c5
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-07-05 22:27:59 +0400

    add js file

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.getElementById('burger-menu');
+    const menuUl = document.getElementById('menu-ul');
+    const line = document.getElementById('line');
+
+    burger.addEventListener("click", function() {
+        menuUl.classList.toggle("menu-burg");
+        line.classList.toggle("line");
+        burger.classList.toggle("lin"); 
+
+    });    
+        document.addEventListener("click", function(event) {
+            if (!burger.contains(event.target)) {
+                menuUl.classList.remove("menu-burg");
+                line.classList.remove("line");
+                burger.classList.remove("lin");
+                
+            }
+    });
+});

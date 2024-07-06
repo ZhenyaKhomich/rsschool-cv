b212923cccf069706a6fc3c95de01a330c6f33d5
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-07-06 12:40:08 +0400

    correct js

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuUl = document.getElementById('menu-ul');
     const line = document.getElementById('line');
 
+    menuUl.classList.remove("menu-burg");
+    line.classList.remove("line");
+    burger.classList.remove("lin");
+
     burger.addEventListener("click", function() {
         menuUl.classList.toggle("menu-burg");
         line.classList.toggle("line");
         burger.classList.toggle("lin"); 
-
-    });    
+    }); 
+       
         document.addEventListener("click", function(event) {
             if (!burger.contains(event.target)) {
                 menuUl.classList.remove("menu-burg");
